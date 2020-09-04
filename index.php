e14e5bd40634c3ca504243a360bf1650d667b734
JOAraka <69601408+JOAraka@users.noreply.github.com>
2020-09-04 14:22:39 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
-<html>
-<head><title> Hello you all</title></head>
-<body>
-<p> Welcome
-
-</body?
-</html>
-<?
+<?php
+  echo "<html>
+            <head><title><php added edit</title></head>
+            <body>
+            
+            <form>
+              <input type="text" name="names" method= "post" placeholder="what is your name"/>
+              <input type="submit" name= "submit" value= "submit"/>
+              
+            </form>
+            </body>
+         </html> "; 
+echo 
+  ?>
